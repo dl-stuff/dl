a2976d34c967df541cb919c5085b40e4a0cb9250
mushy <wildshinobu@gmail.com>
2020-08-18 10:14:48 -0400

    amane

--- adv/amane.py
+++ adv/amane.py
@@ -4,16 +4,16 @@ def module():
     return Amane
 
 class Amane(Adv):
-    a3 = ('bk',0.2)
-    a1 = ('prep',0.75)
+    a1 = [('prep',1.00), ('scharge_all', 0.05)]
+    a3 = ('bk',0.35)
     
     conf = {}
     conf['acl'] = """
         `dragon
         `s2
         `s1
-        `s3, x=5
-        `s4, x=5
+        `s3
+        `s4
         """
     coab = ['Blade','Halloween_Elisanne','Peony']
     share = ['Ranzal']
@@ -25,8 +25,12 @@ class Amane(Adv):
             self.dmg_make(e.name, 4.92)
             
             for _ in range(min(self.buffcount, 2)):
-                self.dmg_make(f'o_{e.name}_boost',0.35)
+                self.dmg_make(e.name, 0.35)
                 self.add_hits(1)
+
+    def s2_proc(self, e):
+        if self.mod('maxhp') < 1.30:
+            Selfbuff('s2_hp', 0.15, -1, 'maxhp', 'buff').on()
 
 if __name__ == '__main__':
     from core.simulate import test_with_argv
