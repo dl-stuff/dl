ce35e725530b5ed7c484b413fea65ef45f5cedbe
mushy <wildshinobu@gmail.com>
2020-08-25 15:44:33 -0400

    revert ranzal

--- adv/ranzal.py
+++ adv/ranzal.py
@@ -18,7 +18,7 @@ class Ranzal(Adv):
         `s4
         """
     coab = ['Blade','Dragonyule_Xainfried','Eleonora']
-    share = ['Sazanka']
+    share = ['Curran']
     conf['afflict_res.poison'] = 0
     
     def s1_proc(self, e):
