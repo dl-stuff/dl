78e86a2ccac2f6f45be846984b4dc6481f9591c3
khalidakh <khaldood@gmail.com>
2020-08-31 08:21:06 +0400

    Update nevin.py

--- adv/nevin.py
+++ adv/nevin.py
@@ -20,14 +20,20 @@ class Nevin(Adv):
 
     conf = nevin_conf.copy()
     conf['slots.d'] = Ramiel()
-    conf['slots.a'] = Twinfold_Bonds()+Howling_to_the_Heavens()
+    conf['slots.a'] = Twinfold_Bonds()+The_Red_Impulse()
     conf['slots.poison.a'] = Twinfold_Bonds()+The_Plaguebringer()
     conf['acl'] = """
-        `dragon.act('c3 s end'), x=5
+        `dragon.act('c3 s end'), (x=5 and not self.unlocked) or (self.unlocked and x=6)
         `s3, not self.s3_buff
-        `s1
-        `s2, s=1
+        if not self.unlocked
+        `s1, 
+        `s2, s=1 
         `s4, x=5
+        else
+        `s1
+        `s2, cancel
+        `s4, x=6
+        end
         """
     coab = ['Curran','Ieyasu','Forte']
     conf['afflict_res.poison'] = 0
