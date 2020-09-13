35d298a21556014ffe6250a5d14464d8c8f65b6a
mushy <wildshinobu@gmail.com>
2020-09-12 21:36:48 -0400

    chelsea

--- adv/chelsea.py
+++ adv/chelsea.py
@@ -20,15 +20,15 @@ class Chelsea(Adv):
 
     def prerun(self):
         Event('dragon').listener(self.s2_clear)
-        self.s2_buffs = []
         Event('s').listener(self.s_hp_check, order=0)
+        self.s2_buffs = []
 
     def s2_clear(self, e):
         for b in self.s2_buffs:
             b.off()
         self.s2_buffs = []
 
-    def x_proc(self, e):
+    def x_before(self, e):
         if self.obsession:
             self.add_hp(-3)
 
@@ -36,11 +36,12 @@ class Chelsea(Adv):
         if self.obsession and e.name in self.damage_sources:
             self.add_hp(-3)
 
+    @property
     def obsession(self):
         return len(self.s2_buffs)
 
     def s2_proc(self, e):
-        self.s2_buffs.append(Selfbuff('s2_obsession',0.3,60).on())
+        self.s2_buffs.append(Selfbuff('s2_obsession',0.3,60,'att','buff').on())
 
 if __name__ == '__main__':
     from core.simulate import test_with_argv
