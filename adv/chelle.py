4ba0ac969fa9541ad8359f9889e6993da043161f
mushy <wildshinobu@gmail.com>
2020-10-22 09:46:54 -0400

    chelle

---
+++ adv/chelle.py
@@ -0,0 +1,10 @@
+from core.advbase import *
+
+class Chelle(Adv):
+    def prerun(self):
+        Event('dragon_end').listener(self.shift_end_inspiration)
+
+    def shift_end_inspiration(self, e):
+        self.inspiration.add(5)
+
+variants = {None: Chelle}
