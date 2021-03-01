edbeb91d5c90eff90583f8cfbef4497d4af3d25f
mushymato <wildshinobu@gmail.com>
2021-02-28 20:36:06 -0500

    uses combo flag

--- adv/ilia.py
+++ adv/ilia.py
@@ -7,15 +7,14 @@ class Ilia(Adv):
         self.alchemy = 0
         self.cartridge = 0
         o_s2_check = self.a_s_dict["s2"].check
-        self.a_s_dict["s2"].check = lambda: o_s2_check() and (
-            self.alchemy > 33 or self.cartridge > 0
-        )
+        self.a_s_dict["s2"].check = lambda: o_s2_check() and (self.alchemy > 33 or self.cartridge > 0)
         self.cartridge_fs = [
             FSAltBuff("s2_cartridge", "cartridge1", uses=1),
             FSAltBuff("s2_cartridge", "cartridge2", uses=1),
             FSAltBuff("s2_cartridge", "cartridge3", uses=1),
         ]
         self.cartridge_t = Timer(self.l_cartridge_timeout)
+        self.uses_combo = True
 
     def alchemy_bars(self):
         if self.alchemy < 33:
