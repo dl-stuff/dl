a902ebe0205e7313a22ceaf36bf8fb72c7dfa91d
Mushymato <wildshinobu@gmail.com>
2021-04-03 08:09:19 +0000

    fix yasu

--- adv/ieyasu.py
+++ adv/ieyasu.py
@@ -12,7 +12,7 @@ class Ieyasu(Adv):
         self.s2_buff = Selfbuff("s2", 0.20, 15, "crit")
         self.s2_buff.modifier.get = self.s2_ifbleed
 
-    def s2_proc(self):
+    def s2_proc(self, e):
         if self.nihilism:
             return
         self.s2_buff.on()
