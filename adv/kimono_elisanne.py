c5ad93e05676a0934bae80626b6be5951bf00ee9
mushymato <wildshinobu@gmail.com>
2021-04-07 13:15:11 -0400

    use multilevel buff for kelly

--- adv/kimono_elisanne.py
+++ adv/kimono_elisanne.py
@@ -2,42 +2,22 @@ from core.advbase import *
 
 
 class Kimono_Elisanne(Adv):
-    def override_effect_off(self, buff):
-        o_effect_off = buff.effect_off
-
-        def effect_off():
-            self.update_ny_level(0)
-            o_effect_off()
-
-        buff.effect_off = effect_off
-
     def prerun(self):
-        self.ny_buff = [
-            ElementalTeambuff("s1_ny_lv1", 0.15, 15, "att", "buff", "flame"),
-            ElementalTeambuff("s1_ny_lv2", 0.25, 30, "att", "buff", "flame"),
-            ElementalTeambuff("s1_ny_lv3", 0.40, 60, "att", "buff", "flame"),
-        ]
-        for buff in self.ny_buff:
-            self.override_effect_off(buff)
+        self.ny_buff = MultiLevelBuff(
+            "ny_prayer",
+            [
+                ElementalTeambuff("ny_prayer_lv1", 0.15, 15, "att", "buff", "flame"),
+                ElementalTeambuff("ny_prayer_lv2", 0.25, 30, "att", "buff", "flame"),
+                ElementalTeambuff("ny_prayer_lv3", 0.40, 60, "att", "buff", "flame"),
+            ],
+        )
         self.ny_level = 0
-        self.ny_haste = [
-            Modifier("a1_haste", "sp", "passive", 0.07),
-            Modifier("a1_haste", "sp", "passive", 0.08),
-            Modifier("a1_haste", "sp", "passive", 0.10),
-        ]
+        self.ny_haste = Modifier("a1_haste", "sp", "passive", 0).on()
+        self.ny_haste.get = self.get_a1_haste
         self.s2_autocharge = self.s2.autocharge_init(276)
 
-    def update_ny_level(self, new_level):
-        self.ny_level = new_level
-        for i, mod in enumerate(self.ny_haste):
-            if i == new_level - 1:
-                mod.on()
-            elif mod.get():
-                mod.off()
-        if new_level == 3:
-            self.s2_autocharge.on()
-        else:
-            self.s2_autocharge.off()
+    def get_a1_haste(self):
+        return {0: 0, 1: 0.07, 2: 0.08, 3: 0.1}[self.ny_buff.level]
 
     @allow_acl
     def s(self, n, s1_lv=None):
@@ -48,16 +28,15 @@ class Kimono_Elisanne(Adv):
 
     def s1_proc(self, e):
         try:
-            add_lv = int(e.group[-1])
+            add_lv = 3 - self.ny_buff.level + int(e.group[-1])
         except ValueError:
             add_lv = 1
-        new_level = min(3, self.ny_level + add_lv)
-        for i, buff in enumerate(self.ny_buff):
-            if i == new_level - 1:
-                buff.on()
-            elif buff.get():
-                buff.off()
-        self.update_ny_level(new_level)
+        for _ in range(add_lv):
+            self.ny_buff.on()
+        if self.ny_buff.level == 3:
+            self.s2_autocharge.on()
+        else:
+            self.s2_autocharge.off()
 
 
 variants = {None: Kimono_Elisanne}
