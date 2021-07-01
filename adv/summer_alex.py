04f22a3fe79311df936bdab0042e68b080a97401
Triskail <ymidoingthis15@gmail.com>
2021-07-01 18:57:29 -0500

    salex if she real

--- adv/summer_alex.py
+++ adv/summer_alex.py
@@ -12,18 +12,19 @@ class Summer_Alex(Adv):
         else:
             self.scorchbloom = self.dmg_formula("s", 14.32)
             self.scorchbloom_rend = self.dmg_formula("afflic", 0.416)
+            self.scorchbloom_rate = self.afflics.scorchrend.rate
+            self.scorchbloom_time = self.afflics.scorchrend.time
         self.sb_t.name = e.name
         self.sb_t.on(30)
 
     def scorchbloom_proc(self, t):
         if self.scorchbloom > 0:
             self.dmg_make(f"{t.name}_scorchbloom", self.scorchbloom, fixed=True)
-            r = self.afflics.scorchrend.on(t.name, 120, 0)
-            scorch_dot = Dot(f"{t.name}_scorchbloom_scorchrend", 0, 21, 2.9)
-            scorch_dot.on()
-            scorch_dot.tick_dmg = self.scorchbloom_rend * r
+            self.afflics.scorchrend.on(t.name, self.scorchbloom_rate, 0, dmg_override=self.scorchbloom_rend, time_override=self.scorchbloom_time)
             self.scorchbloom = 0
             self.scorchbloom_rend = 0
+            self.scorchbloom_rate = 0
+            self.scorchbloom_time = 0
             self.sb_t.off()
             return True
         return False
