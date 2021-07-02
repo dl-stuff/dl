23dddb79fee44dc0f25e883b3b8799b1a28b218a
mushymato <wildshinobu@gmail.com>
2021-07-02 11:25:21 -0400

    fix scorchbloom proc

--- adv/summer_alex.py
+++ adv/summer_alex.py
@@ -2,6 +2,17 @@ from core.advbase import *
 
 
 class Summer_Alex(Adv):
+    conf = {
+        "acl": """
+            `dragon(c2-s-c2-c2-c2-c2-end), cancel
+            `s3, not buff(s3)
+            `s2
+            `s1, s2.charged < 4000 or s=2
+            `s4, cancel
+            `fs, x=5
+        """
+    }
+
     def prerun(self):
         self.scorchbloom = 0
         self.sb_t = Timer(self.scorchbloom_proc)
@@ -17,10 +28,10 @@ class Summer_Alex(Adv):
         self.sb_t.name = e.name
         self.sb_t.on(30)
 
-    def scorchbloom_proc(self, t):
+    def scorchbloom_proc(self, t=None):
         if self.scorchbloom > 0:
-            self.dmg_make(f"{t.name}_scorchbloom", self.scorchbloom, fixed=True)
-            self.afflics.scorchrend.on(t.name, self.scorchbloom_rate, 0, dmg_override=self.scorchbloom_rend, time_override=self.scorchbloom_time)
+            self.dmg_make(f"s2_scorchbloom", self.scorchbloom, fixed=True)
+            self.afflics.scorchrend.on("s2", self.scorchbloom_rate, 0, dmg_override=self.scorchbloom_rend, time_override=self.scorchbloom_time)
             self.scorchbloom = 0
             self.scorchbloom_rend = 0
             self.scorchbloom_rate = 0
@@ -29,9 +40,8 @@ class Summer_Alex(Adv):
             return True
         return False
 
-    def s1_hit2(self, name, base, group, aseq):
-        if group == "enhanced":
-            self.scorchbloom_proc()
+    def s1_enhanced_hit2(self, name, base, group, aseq):
+        self.scorchbloom_proc()
 
 
 variants = {None: Summer_Alex}
