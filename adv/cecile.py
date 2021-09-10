7c8ea8e18997f269158ae5d25ea60dbcd2da03c6
Triskail <ymidoingthis15@gmail.com>
2021-09-10 14:40:01 -0500

    cecile stuff

--- adv/cecile.py
+++ adv/cecile.py
@@ -19,14 +19,17 @@ class Cecile(Adv):
         Event("dragon_end").listener(self.dshift_manachew_gauge)
 
     def a1_update(self, gauge):
-        if not self.manachew_mode.get():
+        if self.manachew_mode.get():
+            max_add = self.manachew_mode.duration - (self.manachew_mode.timer.timing - now())
+            add_time = min(gauge / 10000 * self.manachew_mode.duration, max_add)
+            self.manachew_mode.add_time(add_time)
+        else:
             self.manachew_gauge += gauge
             if self.manachew_gauge == 10000:
                 self.manachew_mode.on()
                 self.manachew_gauge = 0
 
     def dshift_manachew_gauge(self, e):
-    # wait can you dshift during manachew to add time? todo: figure that out
         self.a1_update(5000)
 
     def hitattr_make(self, name, base, group, aseq, attr, onhit=None):
@@ -43,4 +46,17 @@ class Cecile(Adv):
         self.charge_p("a3", 0.2, target="s2")
 
 
-variants = {None: Cecile}
+class Cecile_DODGE(Cecile):
+    def prerun(self):
+        super().prerun()
+        Event("dodge").listener(self.a1_a3_dodge)
+        self.comment = "enable a1/a3 dodge every 15 seconds"
+
+    def a1_a3_dodge(self, e):
+        if not self.is_set_cd("a1_a3", 15):
+            self.a3_dodge_gauge_fill()
+            if self.manachew_mode.get():
+                self.a1_add_manachew_time()
+
+
+variants = {None: Cecile, "DODGE": Cecile_DODGE}
