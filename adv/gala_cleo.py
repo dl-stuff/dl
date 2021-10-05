8d636e6aa85129e7e4c17b495aeacec9b2254507
mushymato <wildshinobu@gmail.com>
2021-10-05 00:02:10 -0400

    forgot ycass

--- adv/gala_cleo.py
+++ adv/gala_cleo.py
@@ -2,10 +2,10 @@ from core.advbase import *
 from module.template import LowerMCAdv
 
 
-class Gala_Cleo_54MC(LowerMCAdv):
-    MC = 54
+class Gala_Cleo_52MC(LowerMCAdv):
+    MC = 52
     NO_DEPLOY = True
     SAVE_VARIANT = False
 
 
-variants = {None: Adv, "50MC": LowerMCAdv, "54MC": Gala_Cleo_54MC}
+variants = {None: Adv, "50MC": LowerMCAdv, "52MC": Gala_Cleo_52MC}
