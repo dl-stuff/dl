a067f62ed056b11f95d47a26e1cc9d9576c3d0ab
mushymato <wildshinobu@gmail.com>
2021-11-29 23:15:47 -0500

    gala gatov & nevin

---
+++ adv/gala_gatov.py
@@ -0,0 +1,37 @@
+from conf import DEFAULT
+from core.advbase import *
+
+
+class Gala_Gatov(Adv):
+    def prerun(self):
+        self.charge_p("prep", 100, target="s2")
+        self.buff2025 = 0
+        self.dualsword_mode = ModeManager(group="dualsword", x=True, fs=True, s1=True, s2=True, duration=33.33, pause=("s", "dragon"))
+
+    @property
+    def dualsword(self):
+        return self.dualsword_mode.timeleft()
+
+    def s1_before(self, e):
+        if e.group == "dualsword":
+            self.current_s["s2"] = "dualswordboosted"
+        elif e.group == DEFAULT:
+            self.dualsword_mode.on()
+            self.charge_p("prep", 100, target=("s1", "s2"))
+
+    def s1_proc(self, e):
+        if e.group == "dualswordboosted":
+            self.current_s["s1"] = "dualsword"
+            self.buff2025 = 0
+
+    def s2_before(self, e):
+        if e.group == "dualsword":
+            self.current_s["s1"] = "dualswordboosted"
+
+    def s2_proc(self, e):
+        if e.group == "dualswordboosted":
+            self.current_s["s2"] = "dualsword"
+            self.buff2025 = 0
+
+
+variants = {None: Gala_Gatov}
