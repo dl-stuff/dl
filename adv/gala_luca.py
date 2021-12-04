7109723bc76d5055e1ca1cc9d36512e8c7b74559
Triskail <ymidoingthis15@gmail.com>
2021-12-04 05:28:47 -0600

    testing junk

--- adv/gala_luca.py
+++ adv/gala_luca.py
@@ -9,6 +9,8 @@ class Gala_Luca(Adv):
     def prerun(self):
         self.crit_mod = self.custom_crit_mod
         self.a1_buff_types = 3
+        self.a1_buff_duration = 20.0
+        self.a1_buff_cooldown = 3.0
         self.a1_states = {(None,) * self.a1_buff_types: 1.0}
         self.all_icon_avg = (0, 0)
         self.s1_icon_avg = (0, 0)
@@ -51,7 +53,7 @@ class Gala_Luca(Adv):
         return min(icon_count, 7)
 
     def custom_crit_mod(self, name):
-        in_s1 = name[0:2] == "s1" or name == "ds"
+        in_s1 = name[0:2] == "s1" or name[0:2] == "ds"
         base_icon_count = self.buff_icon_count()
 
         if name == "test" or self.nihilism:
@@ -69,7 +71,7 @@ class Gala_Luca(Adv):
 
         icon_avg = 0
         for start_state, state_p in self.a1_states.items():
-            state = tuple([b if b is not None and t - b <= 20.0 else None for b in start_state])  # expire old stacks
+            state = tuple([b if b is not None and t - b <= self.a1_buff_duration else None for b in start_state])  # expire old stacks
             a1_buff_count = sum(b is not None for b in state)  # active a1buff count
             icon_count = min(base_icon_count + a1_buff_count, 7)
             current_rate = min(
@@ -83,7 +85,7 @@ class Gala_Luca(Adv):
             mean_rate += current_rate * state_p
             icon_avg += icon_count * state_p
 
-            if state[0] is not None and t - state[0] < 3.0:  # proc in last 3 seconds
+            if state[0] is not None and t - state[0] < self.a1_buff_cooldown:  # proc in last 3 seconds
                 new_states[state] += state_p  # state won't change
             else:
                 miss_rate = 1.0 - current_rate
@@ -101,7 +103,10 @@ class Gala_Luca(Adv):
         return 1.0 + mean_rate * crit_dmg
 
     def post_run(self, end):
-        self.comment = f"avg buff icon {self.all_icon_avg[1]:.2f} (s1 {self.s1_icon_avg[1]:.2f})"
+        if self.comment:
+            self.comment += f"; avg buff icon {self.all_icon_avg[1]:.2f} (s1 {self.s1_icon_avg[1]:.2f})"
+        else:
+            self.comment = f"avg buff icon {self.all_icon_avg[1]:.2f} (s1 {self.s1_icon_avg[1]:.2f})"
 
 
 class Gala_Luca_MAX(Gala_Luca):
@@ -115,4 +120,54 @@ class Gala_Luca_50MC(Gala_Luca, LowerMCAdv):
     CONNECTING_CALL_CRITR = 0.03
 
 
-variants = {None: Gala_Luca, "MAX": Gala_Luca_MAX, "50MC": Gala_Luca_50MC}
+class Gala_Luca_Fast(Gala_Luca):
+    NO_DEPLOY = True
+    comment = "faster but less accurate crit estimation"
+
+    def prerun(self):
+        super().prerun()
+        self.a1_buff_count = 0.0
+        self.a1_cooldown_rate = 0.0
+        self.prev_t = 0.0
+
+    def custom_crit_mod(self, name):
+        in_s1 = name[0:2] == "s1" or name[0:2] == "ds"
+        base_icon_count = self.buff_icon_count()
+
+        if name == "test" or self.nihilism:
+            if self.nihilism:
+                self.all_icon_avg = self.update_icon_avg(base_icon_count, *self.all_icon_avg)
+                if in_s1:
+                    self.s1_icon_avg = self.update_icon_avg(base_icon_count, *self.s1_icon_avg)
+            return self.solid_crit_mod(name)
+
+        base_rate, crit_dmg = self.combine_crit_mods()
+        crit_dmg -= 1
+        t = now()
+        icon_avg = 0
+
+        def decay(value, ratio):
+            return value * (1.0 - ratio ** 0.5)
+
+        t_diff = t - self.prev_t
+        self.prev_t = t
+        self.a1_buff_count = decay(self.a1_buff_count, t_diff / self.a1_buff_duration)
+        self.a1_cooldown_rate = decay(self.a1_cooldown_rate, t_diff / self.a1_buff_cooldown)
+
+        icon_count = min(base_icon_count + self.a1_buff_count, 7)
+        current_rate = min(
+            1.0,
+            base_rate + self.CONNECTING_CALL_CRITR * self.a1_buff_count + min(0.28, 0.04 * icon_count) + 0.1 * icon_count * int(in_s1),
+        )
+        proc_rate = current_rate * (1 - self.a1_cooldown_rate)
+        self.a1_buff_count = min(self.a1_buff_count + proc_rate, self.a1_buff_types)
+        self.a1_cooldown_rate = min(self.a1_cooldown_rate + proc_rate, 1.0)
+
+        self.all_icon_avg = self.update_icon_avg(icon_count, *self.all_icon_avg)
+        if in_s1:
+            self.s1_icon_avg = self.update_icon_avg(icon_count, *self.s1_icon_avg)
+
+        return 1.0 + current_rate * crit_dmg
+
+
+variants = {None: Gala_Luca, "MAX": Gala_Luca_MAX, "50MC": Gala_Luca_50MC, "FAST": Gala_Luca_Fast}
