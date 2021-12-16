b96ace3438a0edc6f68eb8804a2a1b1807346eff
Mushymato <wildshinobu@gmail.com>
2021-12-16 01:51:06 +0000

    add 50mc victor variant

--- adv/victor.py
+++ adv/victor.py
@@ -1,8 +1,13 @@
 from core.advbase import *
+from module.template import LowerMCAdv
 
 
 class Victor_RNG(Adv):
     conf = {"mbleed": False}
 
 
-variants = {None: Adv, "RNG": Victor_RNG}
+class Victor_50MC(LowerMCAdv):
+    pass
+
+
+variants = {None: Adv, "50MC": Victor_50MC, "RNG": Victor_RNG}
