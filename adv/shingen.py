2897e926cbee3fbb6568c74c0c7a6ef5baf961d4
mushymato <wildshinobu@gmail.com>
2022-01-02 23:03:40 -0500

    fix shingen skill share

--- adv/shingen.py
+++ adv/shingen.py
@@ -6,7 +6,7 @@ class Shingen(Adv):
     comment = "always use as many taps as possible for s1"
 
     def prerun(self):
-        self.heal_burst = BurstGambit("heal_burst", 30, self.heal_burst_on, self.heal_burst_off)
+        self.shingen_heal_burst = BurstGambit("heal_burst", 30, self.heal_burst_on, self.heal_burst_off)
         self.furinkazan_mode = ModeManager(
             "furinkazan_mode",
             group="furinkazan",
@@ -20,6 +20,13 @@ class Shingen(Adv):
         )
         self.fervor = 0
 
+    @staticmethod
+    def prerun_skillshare(adv, dst):
+        adv.rebind_function(Shingen, "heal_burst_on")
+        adv.rebind_function(Shingen, "heal_burst_off")
+        adv.shingen_heal_burst = BurstGambit("heal_burst", 30, adv.heal_burst_on, adv.heal_burst_off)
+        adv.fervor = 0
+
     def heal_burst_on(self):
         self.heal_value = self.heal_formula("heal_burst", 140)
 
@@ -28,7 +35,7 @@ class Shingen(Adv):
         self.heal_value = 0
 
     def s1_before(self, e):
-        self.heal_burst.on()
+        self.shingen_heal_burst.on()
 
     def s1_proc(self, e):
         if e.group == DEFAULT and self.fervor == 3:
