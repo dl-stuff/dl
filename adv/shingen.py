08f1e3e7632cced2d49e818f71b2b1e793bf32bd
mushymato <wildshinobu@gmail.com>
2022-01-03 20:03:20 -0500

    fix shingen s1 frames, rename fervor to mettle, allow fs to always counter

--- adv/shingen.py
+++ adv/shingen.py
@@ -3,14 +3,14 @@ from core.advbase import *
 
 
 class Shingen(Adv):
-    comment = "always use as many taps as possible for s1"
+    comment = "always use as many taps as possible for s1; fs always counters"
 
     def prerun(self):
         self.shingen_heal_burst = BurstGambit("heal_burst", 30, self.heal_burst_on, self.heal_burst_off)
         self.furinkazan_mode = ModeManager(
             "furinkazan_mode",
             group="furinkazan",
-            buffs=[Selfbuff("furinkazen_att", 0.2, 30, "att", "passive"), Selfbuff("furinkazen_actdown", 0.5, 30, "ex", "actdown")],
+            buffs=[],
             duration=30,
             x=True,
             s1=True,
@@ -18,7 +18,7 @@ class Shingen(Adv):
             source="s1",
             pause=("s",),
         )
-        self.fervor = 0
+        self.mettle = 0
 
     @staticmethod
     def prerun_skillshare(adv, dst):
@@ -26,7 +26,16 @@ class Shingen(Adv):
         adv.rebind_function(Shingen, "heal_burst_on")
         adv.rebind_function(Shingen, "heal_burst_off")
         adv.shingen_heal_burst = BurstGambit("heal_burst", 30, adv.heal_burst_on, adv.heal_burst_off)
-        adv.fervor = 0
+        adv.mettle = 0
+
+    @allow_acl
+    def s(self, n):
+        if not self.furinkazan_mode.get():
+            if self.mettle == 0:
+                self.current_s["s1"] = DEFAULT
+            else:
+                self.current_s["s1"] = f"mettle{self.mettle}"
+        return super().s(n)
 
     def heal_burst_on(self):
         self.heal_value = self.heal_formula("heal_burst", 140)
@@ -37,13 +46,9 @@ class Shingen(Adv):
 
     def s1_before(self, e):
         self.shingen_heal_burst.on()
-        if e.group == DEFAULT and self.fervor == 3:
-            self.fervor = 0
-            self.furinkazan_mode.on()
 
-    def s2_proc(self, e):
-        if e.group == DEFAULT:
-            self.fervor = min(3, self.fervor + 1)
+    def s1_mettle3_hit3(self, *args):
+        self.furinkazan_mode.on()
 
 
 variants = {None: Shingen}
